5062cfe9f6bcea2f8533a777ccbe8b21550a71f9
Kamil Zielinski <kamilziel@tlen.pl>
2023-03-16 14:59:27 +0100

    Initial commit

---
+++ src/test/java/pl/zielinski/kamil/producttracker/ProductTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zielinski.kamil.producttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
